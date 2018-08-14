9124d4a47f8e0c3e4ffba445f6076adfbca982bc
wangjianzhou <879595361@qq.com>
2018-08-14 18:19:05 +0800

    数据类

---
+++ app/src/main/java/com/tiny/demo/firstlinecode/kotlin/primer/project08/UserKt.kt
@@ -0,0 +1,10 @@
+package com.tiny.demo.firstlinecode.kotlin.primer.project08
+
+/**
+ * @Description: $desc$
+ *
+ * @Author wangjianzhou@qding.me
+ * @Version $version$
+ * @Date 2018/8/14 6:07 PM
+ */
+data class UserKt(var id: Int, var name: String)
