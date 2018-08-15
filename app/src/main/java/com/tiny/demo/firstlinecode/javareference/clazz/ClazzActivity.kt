2131922d6f67d6361084953d51f6ba1df33ccc78
wangjianzhou <879595361@qq.com>
2018-08-15 16:01:42 +0800

    getName getSimpleName getCanonicalName

---
+++ app/src/main/java/com/tiny/demo/firstlinecode/javareference/clazz/ClazzActivity.kt
@@ -0,0 +1,12 @@
+package com.tiny.demo.firstlinecode.javareference.clazz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ClazzActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clazz)
+    }
+}
---
+++ app/src/main/java/com/tiny/demo/firstlinecode/javareference/clazz/Name.java
@@ -0,0 +1,10 @@
+package com.tiny.demo.firstlinecode.javareference.clazz;
+
+/**
+ * @Description: $desc$
+ * @Author wangjianzhou@qding.me
+ * @Version $version$
+ * @Date 2018/8/15 3:55 PM
+ */
+public class Name {
+}
