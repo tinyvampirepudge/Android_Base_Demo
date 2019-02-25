0e14e4dc17ffaeb8caa9bb3c0a9f0548de4ab20c
wangjianzhou <tinytongtong@163.com>
2019-02-25 16:03:03 +0800

    依赖版本更新

---
+++ app/src/main/java/com/tiny/demo/firstlinecode/test/view/loopviewpager/LoopViewPager.java
@@ -0,0 +1,94 @@
+package com.jianghu_app.view.viewpager;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Desc:
+ * Created by tiny on 2018/1/23.
+ * Time: 14:38
+ * Version:
+ */
+public class LoopViewPager extends ViewPager {
+    public LoopViewPager(Context context) {
+        super(context);
+        init();
+    }
+
+    public LoopViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        autoPollTask = new AutoPollTask(this);
+    }
+
+    private static final long TIME_AUTO_POLL = 3000L;
+    private static AutoPollTask autoPollTask;
+    private static boolean running; //标示是否正在自动轮询
+    private static boolean canRun;//标示是否可以自动轮询,可在不需要的是否置false
+
+    static class AutoPollTask implements Runnable {
+        private final WeakReference<ViewPager> mReference;
+
+        //使用弱引用持有外部类引用->防止内存泄漏
+        public AutoPollTask(ViewPager reference) {
+            this.mReference = new WeakReference<>(reference);
+        }
+
+        @Override
+        public void run() {
+            ViewPager viewPager = mReference.get();
+            if (viewPager != null && running && canRun) {
+                int currPos = viewPager.getCurrentItem();
+                viewPager.setCurrentItem(currPos + 1, true);
+                viewPager.postDelayed(autoPollTask, TIME_AUTO_POLL);
+            }
+        }
+    }
+
+    //开启:如果正在运行,先停止->再开启
+    public void start() {
+        if (running) {
+            stop();
+        }
+        canRun = true;
+        running = true;
+        postDelayed(autoPollTask, TIME_AUTO_POLL);
+    }
+
+    public void stop() {
+        running = false;
+        removeCallbacks(autoPollTask);
+    }
+
+    /**
+     * onTouchEvent中的事件被viewPager处理过了，有些拦截不到
+     * 所以在这里拦截
+     *
+     * @param ev
+     * @return
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        //只要接收到事件，就停止滚动。
+        if (running) {
+            stop();
+        }
+        switch (ev.getAction()) {
+            //当手指抬起或者划出页面时
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_OUTSIDE:
+                if (canRun) {
+                    start();
+                }
+                break;
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+}
---
+++ app/src/main/java/com/tiny/demo/firstlinecode/test/view/loopviewpager/ScaleDownPageTransformer.java
@@ -0,0 +1,52 @@
+package com.jianghu_app.view.viewpager;
+
+import android.support.v4.view.ViewPager;
+import android.view.View;
+
+/**
+ * Desc:    左右缩放的PageTransformer
+ * Created by tiny on 2018/1/19.
+ * Time: 17:04
+ * Version:
+ */
+
+public class ScaleDownPageTransformer implements ViewPager.PageTransformer {
+    private static final float DEFAULT_MIN_SCALE = 0.85f;
+    private float mMinScale = DEFAULT_MIN_SCALE;
+
+    /**
+     * Apply a property transformation to the given page.
+     *
+     * @param view     Apply the transformation to this page
+     * @param position Position of page relative to the current front-and-center
+     *                 position of the pager. 0 is front and center. 1 is one full
+     *                 page position to the right, and -1 is one page position to the left.
+     */
+    @Override
+    public void transformPage(View view, float position) {
+        float limitLow = -1;
+        float limitMid = 0;
+        float limitHigh = 1;
+
+        if (position < limitLow) {
+            view.setScaleX(mMinScale);
+            view.setScaleY(mMinScale);
+        } else if (position <= limitHigh) { // [-1,1]
+
+            if (position < limitMid) //[-1,0)
+            {
+                float factor = mMinScale + (1 - mMinScale) * (1 + position);
+                view.setScaleX(factor);
+                view.setScaleY(factor);
+            } else//[0,1]
+            {
+                float factor = mMinScale + (1 - mMinScale) * (1 - position);
+                view.setScaleX(factor);
+                view.setScaleY(factor);
+            }
+        } else { // (1,+Infinity]
+            view.setScaleX(mMinScale);
+            view.setScaleY(mMinScale);
+        }
+    }
+}
